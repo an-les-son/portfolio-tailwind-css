2740806f3e5bf1be00a37043e52a9302b7cbf42f
an-les-son <anjar.les.son@gmail.com>
2025-10-02 23:24:07 +0700

    add script

---
+++ animation.js
@@ -0,0 +1,20 @@
+const text = document.querySelector(".animation-text").children;
+textLen = text.length;
+let index = 0;
+
+function animationText() {
+    for (let i = 0; i < textLen; i++) {
+       text[i].classList.add("hidden");
+    }
+    text[index].classList.remove("hidden");
+    if(index == textLen-1){
+        index=0;
+    }
+    else {
+        index++;
+    }
+
+    setTimeout(animationText, 3000);
+}
+
+window.onload=animationText();

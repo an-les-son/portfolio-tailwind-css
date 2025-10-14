88ab7b4562149dea41d2533cff1e6ed4d7ec9619
an-les-son <anjar.les.son@gmail.com>
2025-10-14 21:17:19 +0700

    update all page

--- script/animation.js
+++ script/animation.js
@@ -39,3 +39,25 @@ function getVisitorData() {
 
     document.getElementById("counter").textContent = visitorData.count;
 
+    
+function updateClock() {
+      const now = new Date();
+
+      // Format waktu 
+      const jam = now.getHours().toString().padStart(2, "0");
+      const menit = now.getMinutes().toString().padStart(2, "0");
+      const detik = now.getSeconds().toString().padStart(2, "0");
+
+      // Format tanggal
+      const options = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' };
+      const tanggal = now.toLocaleDateString('en-US', options);
+
+      // Tampilkan ke elemen HTML
+      document.getElementById("clock").textContent = `${jam}:${menit}:${detik}`;
+      document.getElementById("date").textContent = tanggal;
+    }
+
+    // Jalankan setiap detik
+    setInterval(updateClock, 1000);
+    updateClock(); // panggil pertama kali
+
--- script/work.js
+++ script/work.js
@@ -1,16 +1,247 @@
 // click info description
-// UI/UX
 
-const uiux01 = document.querySelector(".uiux01");
+// Description
 const info_desc = document.querySelector(".info-desc");
+const desc01 = document.querySelector(".desc01");
+const desc02 = document.querySelector(".desc02");
+const desc03 = document.querySelector(".desc03");
+const desc04 = document.querySelector(".desc04");
+const desc05 = document.querySelector(".desc05");
 
+const descin = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex id, dignissimos, fuga saepe impedit a harum accusantium quibusdam atque in? Recusandae quis nulla quibusdam harum in minus optio mollitia, iusto impedit corrupti expedita atque perspiciatis ratione quod quam iste sed vitae reprehenderit autem quidem aliquam maiores nemo quas ipsum.";
+
+// UI/UX
+const uiux01 = document.querySelector(".uiux01");
+const uiux02 = document.querySelector(".uiux02");
+const uiux03 = document.querySelector(".uiux03");
+const uiux04 = document.querySelector(".uiux04");
+const uiux05 = document.querySelector(".uiux05");
+const uiux06 = document.querySelector(".uiux06");
+const uiux07 = document.querySelector(".uiux07");
+const uiux08 = document.querySelector(".uiux08");
+
+// Fullstack Web Development
+const fsw01 = document.querySelector(".fsw01");
+const fsw02 = document.querySelector(".fsw02");
+const fsw03 = document.querySelector(".fsw03");
+const fsw04 = document.querySelector(".fsw04");
+
+// Sketch
+const sketch01 = document.querySelector(".sketch01");
+const sketch02 = document.querySelector(".sketch02");
+const sketch03 = document.querySelector(".sketch03");
+const sketch04 = document.querySelector(".sketch04");
+const sketch05 = document.querySelector(".sketch05");
+const sketch06 = document.querySelector(".sketch06");
+const sketch07 = document.querySelector(".sketch07");
+const sketch08 = document.querySelector(".sketch08");
+
+
+// Description UI/UX
 uiux01.addEventListener("click", function() {
     info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="UI/UX project 01";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Visual Direction</b><br>"+descin;
+    desc03.innerHTML= "<b>Tipografi</b><br>"+descin;
+    desc04.innerHTML= "<b>User Experience</b><br>"+descin;
+    desc05.innerHTML= "<b>Design Objective</b><br>"+descin;
+});
+
+uiux02.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="UI/UX project 02";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Visual Direction</b><br>"+descin;
+    desc03.innerHTML= "<b>Tipografi</b><br>"+descin;
+    desc04.innerHTML= "<b>User Experience</b><br>"+descin;
+    desc05.innerHTML= "<b>Design Objective</b><br>"+descin;
+});
+
+uiux03.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="UI/UX project 03";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Visual Direction</b><br>"+descin;
+    desc03.innerHTML= "<b>Tipografi</b><br>"+descin;
+    desc04.innerHTML= "<b>User Experience</b><br>"+descin;
+    desc05.innerHTML= "<b>Design Objective</b><br>"+descin;
+});
+
+uiux04.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="UI/UX project 04";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Visual Direction</b><br>"+descin;
+    desc03.innerHTML= "<b>Tipografi</b><br>"+descin;
+    desc04.innerHTML= "<b>User Experience</b><br>"+descin;
+    desc05.innerHTML= "<b>Design Objective</b><br>"+descin;
+});
+
+uiux05.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="UI/UX project 05";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Visual Direction</b><br>"+descin;
+    desc03.innerHTML= "<b>Tipografi</b><br>"+descin;
+    desc04.innerHTML= "<b>User Experience</b><br>"+descin;
+    desc05.innerHTML= "<b>Design Objective</b><br>"+descin;
+});
+
+uiux06.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="UI/UX project 06";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Visual Direction</b><br>"+descin;
+    desc03.innerHTML= "<b>Tipografi</b><br>"+descin;
+    desc04.innerHTML= "<b>User Experience</b><br>"+descin;
+    desc05.innerHTML= "<b>Design Objective</b><br>"+descin;
+});
+
+uiux07.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="UI/UX project 07";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Visual Direction</b><br>"+descin;
+    desc03.innerHTML= "<b>Tipografi</b><br>"+descin;
+    desc04.innerHTML= "<b>User Experience</b><br>"+descin;
+    desc05.innerHTML= "<b>Design Objective</b><br>"+descin;
+});
+
+uiux08.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="UI/UX project 08";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Visual Direction</b><br>"+descin;
+    desc03.innerHTML= "<b>Tipografi</b><br>"+descin;
+    desc04.innerHTML= "<b>User Experience</b><br>"+descin;
+    desc05.innerHTML= "<b>Design Objective</b><br>"+descin;
+});
+
+// Description Fullstack Web Development
+fsw01.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Full-Stack Development Web project 01";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Tools</b><br>Lorem, ipsum, dolor, sit, amet, consectetur";
+    desc03.innerHTML= "";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+fsw02.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Full-Stack Development Web project 02";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Tools</b><br>Lorem, ipsum, dolor, sit, amet, consectetur";
+    desc03.innerHTML= "";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+fsw03.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Full-Stack Development Web project 03";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Tools</b><br>Lorem, ipsum, dolor, sit, amet, consectetur";
+    desc03.innerHTML= "";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+fsw04.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Full-Stack Development Web  project 04";
+    desc01.innerHTML= "<b>Project Overview</b><br>"+descin;
+    desc02.innerHTML= "<b>Tools</b><br>Lorem, ipsum, dolor, sit, amet, consectetur";
+    desc03.innerHTML= "";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+// Description Sketch
+sketch01.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Sketch 01";
+    desc01.innerHTML= "<b>Description</b><br>"+descin;
+    desc02.innerHTML= "<b>Size</b><br>cm x cm";
+    desc03.innerHTML= "<b>Media</b><br>Lorem on ipsum";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+sketch02.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Sketch 02";
+    desc01.innerHTML= "<b>Description</b><br>"+descin;
+    desc02.innerHTML= "<b>Size</b><br>cm x cm";
+    desc03.innerHTML= "<b>Media</b><br>Lorem on ipsum";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+sketch03.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Sketch 03";
+    desc01.innerHTML= "<b>Description</b><br>"+descin;
+    desc02.innerHTML= "<b>Size</b><br>cm x cm";
+    desc03.innerHTML= "<b>Media</b><br>Lorem on ipsum";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+sketch04.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Sketch 04";
+    desc01.innerHTML= "<b>Description</b><br>"+descin;
+    desc02.innerHTML= "<b>Size</b><br>cm x cm";
+    desc03.innerHTML= "<b>Media</b><br>Lorem on ipsum";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+sketch05.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Sketch 05";
+    desc01.innerHTML= "<b>Description</b><br>"+descin;
+    desc02.innerHTML= "<b>Size</b><br>cm x cm";
+    desc03.innerHTML= "<b>Media</b><br>Lorem on ipsum";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+sketch06.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Sketch 06";
+    desc01.innerHTML= "<b>Description</b><br>"+descin;
+    desc02.innerHTML= "<b>Size</b><br>cm x cm";
+    desc03.innerHTML= "<b>Media</b><br>Lorem on ipsum";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+sketch07.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Sketch 07";
+    desc01.innerHTML= "<b>Description</b><br>"+descin;
+    desc02.innerHTML= "<b>Size</b><br>cm x cm";
+    desc03.innerHTML= "<b>Media</b><br>Lorem on ipsum";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
+});
+
+sketch08.addEventListener("click", function() {
+    info_desc.classList.remove("hidden");
+    document.querySelector(".tproject").innerHTML="Sketch 08";
+    desc01.innerHTML= "<b>Description</b><br>"+descin;
+    desc02.innerHTML= "<b>Size</b><br>cm x cm";
+    desc03.innerHTML= "<b>Media</b><br>Lorem on ipsum";
+    desc04.innerHTML= "";
+    desc05.innerHTML= "";
 });
 
 
 // close button info description
-
 const btn_cls = document.querySelector(".btn-cls");
 
 btn_cls.addEventListener("click", function() {
